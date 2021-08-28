d5b0bb09eaf3086fe5ef2bd9bcc57f440e1043f8
Your Name <sanya.seredyak@gmail.com>
2021-08-28 21:25:09 +0300

    Fix permissions for swagger

--- photo_saver/urls.py
+++ photo_saver/urls.py
@@ -48,11 +48,10 @@ urlpatterns = [
     path("saver/", include("app_saver.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += [
-       path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-       path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
-    ]
+urlpatterns += [
+   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
