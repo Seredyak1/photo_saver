631ca9e5f6d1754827de1d3cf28d30b3eea48981
Your Name <sanya.seredyak@gmail.com>
2021-08-28 21:04:02 +0300

    Fix permissions for alive checker

--- photo_saver/views.py
+++ photo_saver/views.py
@@ -4,12 +4,13 @@ from rest_framework.decorators import (
     permission_classes,
     schema,
 )
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 
 @api_view(["GET"])
 @schema(None)
-@authentication_classes([])
-@permission_classes([])
+# @authentication_classes([])
+@permission_classes([AllowAny,])
 def simple_health_check(request):
     return Response()
